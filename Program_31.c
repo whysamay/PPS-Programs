85ef180ee0d0e7da7878ad0f2589d916512da565
whysamay <117376677+whysamay@users.noreply.github.com>
2023-01-07 20:32:14 +0530

    Update Program_31.c

--- Program_31.c
+++ Program_31.c
@@ -5,9 +5,7 @@ int main()
     char str1[20], str2[20];
     printf("Enter your first name: ");
     scanf("%s", str1);
-    printf("Enter your last name: ");
-    scanf("%s", str2);
-    strcat(str1 , str2);
-    printf("%s", str1);
+    strcpy(str2 , str1);
+    printf("%s", str2);
     return 0;
 }
